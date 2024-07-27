ca730952b57a54bd6871c2f7316057ed0eb50d2a
unknown <frogibogidogi@gmail.com>
2024-07-27 22:37:27 +1000

    First window

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('hallo world!')
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Design.ui',self)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())
