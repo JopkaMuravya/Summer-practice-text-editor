f510b1131a3e12297a5b2efab3a4369a050571d4
unknown <frogibogidogi@gmail.com>
2024-07-28 23:09:27 +1000

    The initial stage of saving and opening a file

--- main_window.py
+++ main_window.py
@@ -1,5 +1,5 @@
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QFileDialog, QMessageBox
 
 
 class MainWindow(QMainWindow):
@@ -7,3 +7,25 @@ class MainWindow(QMainWindow):
         super().__init__()
         uic.loadUi('Design.ui', self)
         self.setWindowTitle('TextUra')
+
+        self.actionOpen_file_2.triggered.connect(self.open_file)
+        self.actionSave_file_2.triggered.connect(self.save_file)
+
+    def open_file(self):
+        options = QFileDialog.Options()
+        file_name, _ = QFileDialog.getOpenFileName(self, "Открыть файл", "", "Text Files (*.txt);;All Files (*)", options=options)
+        if file_name:
+            try:
+                with open(file_name, 'r', encoding='utf-8') as file:
+                    self.textEdit.setText(file.read())
+            except Exception as e:
+                QMessageBox.warning(self, 'Ошибка', f'Не удалось открыть файл: {e}')
+
+    def save_file(self):
+        options = QFileDialog.Options()
+        file_name, _ = QFileDialog.getSaveFileName(self, "Сохранить файл", "", "Text Files (*.txt);;All Files (*)", options=options)
+        if file_name:
+            with open(file_name, 'w', encoding='utf-8') as file:
+                text = self.textEdit.toPlainText()
+                file.write(text)
+
